fc630b4dee5cce493aa962b8706793ae6072db22
shantihedelin <shanti.hedelin@chasacademy.se>
2023-11-29 13:23:46 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
